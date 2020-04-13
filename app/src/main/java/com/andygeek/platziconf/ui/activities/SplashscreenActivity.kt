7b6cab983112041ecac07374b8acaff40258546d
AndyGeek <andy.eulogio@gmail.com>
2020-04-13 14:05:10 -0500

    Upload project

---
+++ app/src/main/java/com/andygeek/platziconf/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,13 @@
+package com.andygeek.platziconf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.andygeek.platziconf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
